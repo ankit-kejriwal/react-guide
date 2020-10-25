d6e3f78eb8095a25369b7be9fcde3db48bcced93
ankit <ankitkejriwaal@gmail.com>
2020-10-25 10:10:57 -0400

    added state and event handler in the component

--- src/App.js
+++ src/App.js
@@ -11,20 +11,29 @@ function App() {
     ]
   )
   
-  const switchHandler = () => {
+  const switchHandler = (newName) => {
     
     setPerson([
-      {name:'new_ankit', age:22},
+      {name:newName, age:22},
       {name:'new_kejriwal', age: 23},
+      {name:'new_ABD', age: 21 }
+    ]);
+  }
+
+  const nameChangehHandler = (event) => {
+    
+    setPerson([
+      {name:"ankit123", age:22},
+      {name:event.target.value, age: 23},
       {name:'new_ABD', age: 21 }
     ]);
   }
   return (
     <div className="App">
       <h1>I am a react app</h1>
-      <button onClick={switchHandler}>Click me</button>
+      <button onClick={()=> switchHandler('ankit!!!')}>Click me</button>
       <Person name={person[0].name} age={person[0].age} />
-      <Person name={person[1].name} age={person[1].age} />
+      <Person click={switchHandler} change={nameChangehHandler} name={person[1].name} age={person[1].age} />
       <Person name={person[2].name} age={person[2].age} >Hobbies : Cricket</Person>
     </div>
   );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,9 @@ const person = (props) => {
     const {name, age} = props;
     return(
         <div>
-            <h1>I am  {name} and i am {age} years old</h1>
-            <h1>{props.children}</h1>    
+            <h1 onClick={props.click}>I am  {name} and i am {age} years old</h1>
+            <h1>{props.children}</h1>
+            <input type="text" name="text" id="text" value={props.name} onChange={props.change} />    
         </div>
         
     )
